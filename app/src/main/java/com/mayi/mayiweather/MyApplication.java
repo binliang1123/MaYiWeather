0584a76e3d5a92b15b80f3f8b5d6eb49e65232b2
Liang <604219225@qq.com>
2017-05-27 11:41:40 +0800

    First commit

---
+++ app/src/main/java/com/mayi/mayiweather/MyApplication.java
@@ -0,0 +1,10 @@
+package com.mayi.mayiweather;
+
+import org.litepal.LitePalApplication;
+
+/**
+ * Created by Administrator on 2017/5/27.
+ */
+
+public class MyApplication extends LitePalApplication {
+}
---
+++ app/src/main/java/com/mayi/mayiweather/db/County.java
@@ -0,0 +1,47 @@
+package com.mayi.mayiweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/5/27.
+ */
+
+public class County extends DataSupport {
+
+    private int id;
+    private String countyName;
+    private String cityId;
+    private String weatherId;
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(String cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+}
